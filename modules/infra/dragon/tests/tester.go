f82415f141a95779c168f35f2145ea2a3e38ef96
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-12-07 21:53:09 +0530

    Create tester.go

---
+++ modules/infra/dragon/tests/tester.go
@@ -0,0 +1,59 @@
+package tests
+
+func testURLStatus(method string, url string, statusCode ...int) bool {
+	log.Info("Testing " + url + " with method " + method)
+	testsDone += 1
+	url = siteUrl + url
+	client := http.Client{Timeout: 15 * time.Second}
+	var resp *http.Response
+	var err error
+
+	if method == "GET" {
+		resp, err = client.Get(url)
+	} else if method == "HEAD" {
+		resp, err = client.Head(url)
+	} else {
+		log.Error("FAIL: Invalid method " + method + " in test for URL " + url)
+		return false
+	}
+
+	if err != nil {
+		log.Error("FAIL: " + err.Error())
+		return false
+	}
+
+	if resp.Request.URL.String() == siteUrl+"/maint/page" {
+		log.Error("FAIL: Got maintainance page")
+		return false
+	}
+
+	var checkPassed = false
+	var codes = ""
+	var no5xx = false
+	for i, code := range statusCode {
+		if resp.StatusCode == code || code == 0 {
+			checkPassed = true
+		} else if code == 0 {
+			no5xx = true
+		}
+		if i == 0 {
+			codes += strconv.Itoa(code)
+		} else {
+			codes = codes + "/" + strconv.Itoa(code)
+		}
+	}
+
+	if !checkPassed {
+		log.Error("FAIL: Expected status code " + codes + " but got status code " + strconv.Itoa(resp.StatusCode))
+		return false
+	}
+
+	if no5xx && resp.StatusCode >= 500 {
+		log.Error("FAIL: Got 5xx error: " + strconv.Itoa(resp.StatusCode))
+		return false
+	}
+
+	log.Info("PASS: With status code " + strconv.Itoa(resp.StatusCode))
+	testsSuccess += 1
+	return true
+}
