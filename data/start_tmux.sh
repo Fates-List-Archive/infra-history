8668f9dd52f24239ea02b6c2dee0d7b0fecfa221
cheesycod <cheesycod@outlook.com>
2021-12-09 20:08:20 +0000

    upgrade dragon depa

--- data/start_tmux.sh
+++ data/start_tmux.sh
@@ -1,3 +1,4 @@
+tmux kill-server
 tmux new-session -d -s dragon 'cd ~/FatesList/modules/infra/dragon && ./dragon --cmd dragon.server'
 tmux new-session -d -s manager 'dragon --cmd site.manager'
 tmux new-session -d -s mapleshade 'cd ~/GitHub-Updates-Bot && source ~/.mapleshadecfg && npm start'
