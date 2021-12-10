e9d8a1f4ca7c3a40cd0714c41b19088c2cf0d358
cheesycod <cheesycod@outlook.com>
2021-12-10 16:51:15 +0000

    ingnore history stuff

--- data/start_tmux.sh
+++ data/start_tmux.sh
@@ -1,11 +1,11 @@
 HISTCONTROL="ignorespace${HISTCONTROL:+:$HISTCONTROL}"
 tmux kill-server
 tmux new-session -d -s dragon 
-tmux send-keys -t dragon 'cd ~/FatesList/modules/infra/dragon && ./dragon --cmd dragon.server; exec $SHELL' Enter
+tmux send-keys -t dragon ' cd ~/FatesList/modules/infra/dragon && ./dragon --cmd dragon.server; exec $SHELL' Enter
 tmux new-session -d -s manager 
-tmux send-keys -t manager 'dragon --cmd site.manager; exec $SHELL' Enter
+tmux send-keys -t manager ' dragon --cmd site.manager; exec $SHELL' Enter
 tmux new-session -d -s mapleshade
-tmux send-keys -t mapleshade 'cd ~/GitHub-Updates-Bot && source ~/.mapleshadecfg && npm start; exec $SHELL' Enter
+tmux send-keys -t mapleshade ' cd ~/GitHub-Updates-Bot && source ~/.mapleshadecfg && npm start; exec $SHELL' Enter
 tmux new-session -d -s main 
-tmux send-keys -t main 'dragon --cmd site.run; exec $SHELL' Enter
+tmux send-keys -t main ' dragon --cmd site.run; exec $SHELL' Enter
 export HISTCONTROL
