0549b7875892ae3257cf075b40f072539058a3c1
cheesycod <cheesycod@outlook.com>
2022-01-23 10:56:12 +0000

    remove iframe api

--- modules/discord/internal_sunbeam.py
+++ modules/discord/internal_sunbeam.py
@@ -226,27 +226,3 @@ async def guild_review_page(request: Request, guild_id: int, page: int = 1):
     user = {}
     
     return await templates.TemplateResponse("ext/reviews.html", {"request": request, "data": {"user": user}} | data, context = context)
-
-@router.get("/_sunbeam/pub/profile/{user_id}/settings")
-async def profile_editor(
-    request: Request,
-    user_id: int,
-):
-    viewer = int(request.session.get("user_id", -1))
-    admin = (await is_staff(staff_roles, viewer, 4))[0] if viewer else False
-    personal = user_id == int(request.session.get("user_id", -1))
-    personal = personal or admin
-
-    if not personal:
-        return abort(403)
-
-    # To profile editor, all users are bots due to prior design choices
-    profile = await core_classes.User(id = user_id, db = db).profile(bot_logs = False)
-    context = {
-        "real_user_token": await db.fetchval("SELECT api_token FROM users WHERE user_id = $1", user_id),
-        "mode": "edit",
-        "bot": dict(profile) | profile["profile"],
-        "langs": [{"value": lang.value, "text": lang.__doc__} for lang in list(enums.SiteLang)]
-    }
-
-    return await templates.TemplateResponse("profile_edit.html", {"request": request, "iframe": True} | context, context=context)
